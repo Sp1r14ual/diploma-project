3b18a2ba370182abac63cdec400c67c22e7f38de
sp1r14ual <sp1r14ual@gmail.com>
2025-04-13 22:08:07 +0700

    made IS_ACTIVE flag common for all /house tests

--- tests/6_get_all_houses_test.py
+++ tests/6_get_all_houses_test.py
@@ -4,8 +4,6 @@ import pytest
 from app.settings import settings
 house_test_module = importlib.import_module('tests.3_house_test')
 
-IS_ACTIVE = False
-
 
 def query_house_by_id_client():
     data_to_send = {'id_client': 46831}
@@ -19,7 +17,7 @@ def query_house_by_id_organization():
     return requests.post(house_test_module.URL + '/get_all_houses/for_organization', json=data_to_send)
 
 
-@pytest.mark.skipif(not (IS_ACTIVE and all((settings.DADATA_TOKEN, settings.DADATA_SECRET))), reason="prevent using dadata api")
+@pytest.mark.skipif(not (house_test_module.IS_ACTIVE and all((settings.DADATA_TOKEN, settings.DADATA_SECRET))), reason="prevent using dadata api")
 def test_get_all_houses_for_person():
 
     house_test_module.test_insert_in_house_for_person()
@@ -41,7 +39,7 @@ def test_get_all_houses_for_person():
                 item["id"] for item in response.json()])
 
 
-@pytest.mark.skipif(not (IS_ACTIVE and all((settings.DADATA_TOKEN, settings.DADATA_SECRET))), reason="prevent using dadata api")
+@pytest.mark.skipif(not (house_test_module.IS_ACTIVE and all((settings.DADATA_TOKEN, settings.DADATA_SECRET))), reason="prevent using dadata api")
 def test_get_all_houses_for_organization():
     house_test_module.test_insert_in_house_for_organization()
     response = query_house_by_id_organization()
